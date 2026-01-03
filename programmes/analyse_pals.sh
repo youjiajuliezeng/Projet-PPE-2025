74a5eadb00f8204f35e4da6ec23ce244a8aeb941
Zheng Ting <lina.ting.zheng@gmail.com>
2026-01-03 22:26:01 +0800

    ...

--- programmes/analyse_pals.sh
+++ programmes/analyse_pals.sh
@@ -50,11 +50,9 @@ if [ "$LANG" = "en" ]; then
 
     python3 ./programmes/cooccurrents.py --target "[Dd]ream(s|ed|ing|t)?" "$INPUT_FILE" \
         -N 200 \
-        --context-length 5 \
-        --min-cofrequency 5 \
-        --min-frequency 10 \
+        --context-length 2 \
         --match-mode regex \
-        | grep -v -E "^(the|of|and|to|a|in|that|is|are|was|were|be|been|being|have|has|had|having|with|as|by|for|at|on|but|or|not|it|its|it's|he|him|his|she|her|they|them|their|about|we|can)\s" \
+        | grep -v -E "^(the|of|and|to|a|in|that|is|are|was|were|be|been|being|have|has|had|having|with|as|by|for|at|on|but|or|not|it|its|it's|he|him|his|she|her|they|them|their|about|we|can|your|my|our|someone|3|1|2|4|5|if|which|into|within|may|using|during|between|such|where|most|while|do|how|does|this|use|down|behind|would|cannot|from|work|these|why|an|because|i|only|whether|now|should|when|really|other|some|ever|often|also|another|see|says|still|just|could)\s" \
         > "$OUTPUT_FILE"
 
 elif [ "$LANG" = "fr" ]; then
@@ -66,11 +64,9 @@ elif [ "$LANG" = "fr" ]; then
 
     python3 ./programmes/cooccurrents.py --target "[Rr]êv(e|es|er|ant|a|ait|ée|ées|é|és)" "$INPUT_FILE" \
         -N 200 \
-        --context-length 5 \
-        --min-cofrequency 5 \
-        --min-frequency 10 \
+        --context-length 2 \
         --match-mode regex \
-        | grep -v -E "^(le|la|les|de|des|du|et|à|en|que|qui|dans|pour|sur|avec|par|est|son|ses|ces|cette|pas|plus|comme|mais|ou|où|si|ne|se|ce|il|elle|ils|elles|lui|leur|eux|je|tu|nous|vous|on|un|une|moi|sont|leurs|leur|notre|votre|ma|mon|ta|ton|sa|)\s" \
+        | grep -v -E "^(ai|alors|après|au|aux|avoir|bien|car|ce|cela|celui|ces|cette|chez|comment|dans|de|des|deux|dit|donc|dont|du|elle|elles|en|entre|est|et|était|été|eux|fait|faire|il|ils|je|la|le|lequel|les|leur|leurs|lui|ma|mais|mes|moi|mon|ne|non|nos|notre|nous|on|ont|ou|où|par|pas|peu|peut|pendant|plus|plusieurs|pour|pourquoi|qu|que|qui|sa|sans|se|ses|si|sont|sur|ta|tant|tel|ton|tous|tout|travers|tu|un|une|ve|vos|votre|vous|aussi|son|comme|j|n|serait|être|p|ainsi|3|a|ici|devient|5|quand|contenu|souvent|également|mieux|propos|parfois|4|jamais|cas|2|autres|avec|selon|y|même|c|s|d|à|l)\s" \
         > "$OUTPUT_FILE"
 
 elif [ "$LANG" = "zh" ]; then
@@ -82,11 +78,9 @@ elif [ "$LANG" = "zh" ]; then
 
     python3 ./programmes/cooccurrents.py --target "梦" "$INPUT_FILE" \
         -N 200 \
-        --context-length 5 \
-        --min-cofrequency 5 \
-        --min-frequency 10 \
+        --context-length 4 \
         --match-mode regex \
-        | grep -v -E '^(的|了|是|在|和|与|中|有|为|对|而|但|也|又|并|这个|那个|一种|一些|会|回|"|"|，|里|做)\s' \
+        | grep -v -E '^(的|了|是|在|和|与|中|有|为|对|而|但|也|又|并|这个|那个|一种|一些|会|回|”|“|，|:|《|》|;|(|)|、|。|里|做“|”|虽|如|之|前|场|：|以|一个|自己|《|大|就|一|新|我|都|到|你|不|、|。)\s' \
         > "$OUTPUT_FILE"
 
 else
